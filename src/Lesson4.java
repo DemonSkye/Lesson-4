4147713e3a4f1133d8d0974fdf6639c895be11a3
DemonSkye <damien.bell.27@gmail.com>
2018-08-10 11:22:04 -0400

    Source code for lesson 4

--- src/Lesson4.java
+++ src/Lesson4.java
@@ -1,5 +1,35 @@
 public class Lesson4 {
     public static void main(String args[]){
-        
+
+        //String hello = "Hello";
+        //hello = hello.toLowerCase();
+        //hello = hello.toUpperCase();
+        //System.out.println(hello);
+
+        //Integer a = 5;
+        //String numbers = "12345";
+        //a += Integer.parseInt(numbers);
+        //System.out.println("a= " +a + " -- numbers = " +numbers);
+
+        int year = 18;
+        String currentYear = "The year is 20";
+        currentYear += year;
+        System.out.println(currentYear);
+
+        //Basic types: primatives
+        //int, double, long, boolean, char
+        //integer, floating point number, a long number, true / false, a single character (in single quotes)
+
+        //More advanced types: (wrapper classes)
+        //Integer, Double, Long, Boolean, Character, String
+        //int,double,long,boolean,char, Two or more letters within quotes "Hello"
+
+        //Order of operations P, E, M/D, A/S (Left to right)
+        //If we want to add 1 to a number we have 3 options.
+        //int a = 0;
+        //a = a + 1; //Not going to be used
+        //a += 1; //appending
+        //a++; //incrementing, only by 1
+
     }
 }
